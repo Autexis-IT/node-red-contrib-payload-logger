a63a456a8cdca929bec6cde3858d9c635c2a97b5
Jhe <joel.herren@autexis.com>
2023-10-04 11:24:48 +0200

    implemeneted append function and fixed jsonata for identifier.

--- nodes/logger/logger.js
+++ nodes/logger/logger.js
@@ -1,3 +1,5 @@
+const fs = require('fs-extra');
+const { id } = require('date-fns/locale');
 module.exports = function (RED) {
   const util = require('util');
   const _ = require('lodash');
@@ -8,72 +10,188 @@ module.exports = function (RED) {
     RED.nodes.createNode(this, config);
     const node = this;
     node.on('input', function (msg, send, done) {
-      const name = config.name;
+      const {
+        name,
+        debugWindow,
+        console,
+        appendingToFile,
+        appendNewline,
+      } = config;
       const PAYLOAD_LOGS_PATH = process.env.PAYLOAD_LOGS_PATH || './logs';
       const folder = config.folder || msg.folder;
       if (_.isNil(folder)) {
-        node.error(`folder is undefined`);
+        node.error('folder is undefined');
         return;
       }
-      let prefix = config.prefix || msg.prefix || msg.type;
+      const prefix = config.prefix || msg.prefix || msg.type;
       if (_.isNil(prefix)) {
-        node.error(`prefix is undefined`);
+        node.error('prefix is undefined');
         return;
       }
-      const identifierPath = config.identifier || msg.identifier;
-      const identifier = _.get(msg, identifierPath);
       let extension = config.extensionHidden;
       if (_.isNil(extension)) {
-        node.error(`extension is undefined`);
+        node.error('extension is undefined');
         return;
       }
-      const debugWindow = config.debugWindow;
-      const console = config.console;
 
       function sendDebug(msg) {
         msg = RED.util.encodeObject(msg);
         RED.comms.publish('debug', msg);
       }
 
+      // Write into Console
       function sendConsole(msg) {
         if (typeof msg === 'string') {
           node.log((msg.indexOf('\n') !== -1 ? '\n' : '') + msg);
         } else if (typeof msg === 'object') {
-          node.log('\n' + util.inspect(msg));
+          node.log(`\n${util.inspect(msg)}`);
         } else {
           node.log(util.inspect(msg));
         }
       }
 
-      if (config.extensionHidden === 'str') {
-        extension = config.extension || msg.extension;
-      }
-      let dir
-      if (!_.isNil(identifier)) {
-        dir = path.join(`${PAYLOAD_LOGS_PATH}`, `${folder}`, `${prefix}_${identifier}_${msg._msgid}.${extension}`);
-      } else {
-        dir = path.join(`${PAYLOAD_LOGS_PATH}`, `${folder}`, `${prefix}_${msg._msgid}.${extension}`);
-      }
-      try {
-        fs.ensureDirSync(path.dirname(dir));
-      } catch (err) {
-        node.status({fill: 'red', shape: 'ring', text: `Can't create File (msgid: ${msg._msgid}).`});
-        node.error(err);
-        return;
-      }
-      let logText;
-      try {
-        //using Writestream for future feature to appending payloads to one file.
-        const wstream = fs.createWriteStream(dir, {encoding: 'binary', flags: 'w', autoClose: true});
-        node.wstream = wstream;
-        wstream.on('error', function (err) {
-          node.error(err);
+      // read log file
+      function readfile(dir, callback) {
+        fs.readFile(dir, 'utf8', (err, existingData) => {
+          callback(err, existingData);
         });
-        wstream.on('open', function () {
+      }
 
+      // Write log file
+      function writeFile(dir, logText) {
+        fs.writeFile(dir, logText, (err) => {
+          if (err) throw err;
+        });
+      }
+
+      // Append to file just for json
+      function appendJson(dir, payload) {
+        let logText = payload;
+        fs.exists(dir, (exists) => {
+          if (exists) {
+            readfile(dir, (err, data) => {
+              try {
+                //   const convertData = JSON.parse(data);
+                if (data.startsWith('[')) {
+                  const convertData = JSON.parse(data);
+                  convertData.push(logText);
+                  logText = JSON.stringify(convertData, null, 2);
+                } else {
+                  let newObject;
+                  try {
+                    newObject = [JSON.parse(data)];
+                    // eslint-disable-next-line no-shadow
+                  } catch (err) {
+                    newObject = [];
+                  }
+                  newObject.push(logText);
+                  logText = JSON.stringify(newObject, null, 2);
+                }
+              } catch (jsonError) {
+                node.status({
+                  fill: 'red',
+                  shape: 'ring',
+                  text: `Error while parsing JSON from File. (msgid: ${msg._msgid}).`,
+                });
+                node.error(jsonError);
+                return;
+              }
+              writeFile(dir, logText);
+            });
+          } else {
+            writeFile(dir, JSON.stringify(logText, null, 2));
+          }
+        });
+      }
+
+      function appendFile(dir, logText) {
+        fs.appendFile(dir, logText, (err) => {
+          if (err) throw err;
+        });
+      }
+
+      // Append to file for all other extensions
+      function appendToFile(dir, payload) {
+        let logText = payload;
+        if (appendNewline) {
+          fs.exists(dir, (exists) => {
+            if (exists) {
+              readfile(dir, (err, data) => {
+                if (data.length !== 0) {
+                  logText = `\n${logText}`;
+                }
+                appendFile(dir, logText);
+              });
+            } else {
+              writeFile(dir, logText);
+            }
+          });
+        } else {
+          appendFile(dir, logText);
+        }
+      }
+
+      // reading JSONata
+      const identifierPath = config.identifier || msg.identifier;
+      const preparedEditExpression = RED.util.prepareJSONataExpression(identifierPath, this);
+      RED.util.evaluateJSONataExpression(preparedEditExpression, msg, (error, value) => {
+        const identifier = value;
+
+        if (config.extensionHidden === 'str') {
+          extension = config.extension || msg.extension;
+        }
+        let dir;
+
+        // creating File directory
+        if (!_.isNil(identifier)) {
+          if (appendingToFile === 'true') {
+            dir = path.join(`${PAYLOAD_LOGS_PATH}`, `${folder}`, `${prefix}_${identifier}.${extension}`);
+          } else {
+            dir = path.join(`${PAYLOAD_LOGS_PATH}`, `${folder}`, `${prefix}_${identifier}_${msg._msgid}.${extension}`);
+          }
+        } else {
+          if (appendingToFile === 'true') {
+            dir = path.join(`${PAYLOAD_LOGS_PATH}`, `${folder}`, `${prefix}.${extension}`);
+          } else {
+            dir = path.join(`${PAYLOAD_LOGS_PATH}`, `${folder}`, `${prefix}_${msg._msgid}.${extension}`);
+          }
+        }
+
+        try {
+          fs.ensureDirSync(path.dirname(dir));
+        } catch (err) {
+          node.status({
+            fill: 'red',
+            shape: 'ring',
+            text: `Can't create File (msgid: ${msg._msgid}).`,
+          });
+          node.error(err);
+          return;
+        }
+        let logText;
+        try {
           if (config.logContent === 'payload') {
             try {
-              logText = JSON.stringify(msg.payload);
+              // Just stringify when its a json object
+              if (typeof logText === 'object' && logText !== null) {
+                logText = JSON.stringify(logText);
+              } else {
+                logText = msg.payload;
+              }
+
+              if (appendingToFile === 'true') {
+                if (extension === 'json') {
+                  appendJson(dir, logText);
+                } else {
+                  appendToFile(dir, logText);
+                }
+              } else {
+                if (typeof logText === 'object' && logText !== null) {
+                  logText = JSON.stringify(logText);
+                }
+
+                writeFile(dir, logText);
+              }
               if (debugWindow) {
                 sendDebug({
                   id: node.id,
@@ -82,7 +200,7 @@ module.exports = function (RED) {
                   path: node._flow.path,
                   name: name,
                   topic: msg.topic,
-                  msg: msg.payload
+                  msg: msg.payload,
                 });
               }
               if (console) {
@@ -92,50 +210,77 @@ module.exports = function (RED) {
               node.status({
                 fill: 'red',
                 shape: 'ring',
-                text: `Can't write payload to the file (msgid: ${msg._msgid}).`
+                text: `Can't write payload to the file (msgid: ${msg._msgid}).`,
               });
+              node.error(err);
               return;
             }
           } else {
             try {
-              const expr = RED.util.prepareJSONataExpression(config.logType, this);
-              logText = RED.util.evaluateJSONataExpression(expr, msg);
-              if (debugWindow) {
-                sendDebug({
-                  id: node.id,
-                  z: node.z,
-                  _alias: node._alias,
-                  path: node._flow.path,
-                  name: name,
-                  topic: msg.topic,
-                  msg: logText
-                });
-              }
-              if (console) {
-                sendConsole(logText);
-              }
-              msg.payload = logText;
+              const expr = RED.util.prepareJSONataExpression(config.logType, node);
+              RED.util.evaluateJSONataExpression(expr, msg, (err, jsonatavalue) => {
+                logText = jsonatavalue;
+                if (appendingToFile === 'true') {
+                  if (extension === 'json') {
+                    appendJson(dir, logText);
+                  } else {
+                    appendToFile(dir, logText);
+                  }
+                } else {
+                  writeFile(dir, logText);
+                }
+
+                if (debugWindow) {
+                  sendDebug({
+                    id: node.id,
+                    z: node.z,
+                    _alias: node._alias,
+                    path: node._flow.path,
+                    name: name,
+                    topic: msg.topic,
+                    msg: logText,
+                  });
+                }
+                if (console) {
+                  sendConsole(logText);
+                }
+                msg.payload = logText;
+              });
             } catch (err) {
-              node.status({fill: 'red', shape: 'ring', text: `Error while parsing JSONata (msgid: ${msg._msgid}).`});
+              node.status({
+                fill: 'red',
+                shape: 'ring',
+                text: `Error while parsing JSONata (msgid: ${msg._msgid}).`,
+              });
               return;
             }
           }
-          wstream.end(logText);
-        })
-        node.status({fill: 'green', shape: 'ring', text: `Log successfully created (msgid: ${msg._msgid}).`})
-        setTimeout(() => {
-          node.status({fill: '', shape: '', text: ``});
-        }, 5000);
-
-      } catch (err) {
-        this.status({fill: 'red', shape: 'ring', text: `Can't create log (msgid: ${msg._msgid}).`});
-        node.error(err);
-        return;
-      }
-      node.send(msg);
-      done();
+          node.status({
+            fill: 'green',
+            shape: 'ring',
+            text: `Log successfully created (msgid: ${msg._msgid}).`,
+          });
+          setTimeout(() => {
+            node.status({
+              fill: '',
+              shape: '',
+              text: '',
+            });
+          }, 5000);
+        } catch (err) {
+          this.status({
+            fill: 'red',
+            shape: 'ring',
+            text: `Can't create log (msgid: ${msg._msgid}).`,
+          });
+          node.error(err);
+          return;
+        }
+        node.send(msg);
+        done();
+      });
     });
   }
 
   RED.nodes.registerType('Logger', LoggerNode);
-}
+};
